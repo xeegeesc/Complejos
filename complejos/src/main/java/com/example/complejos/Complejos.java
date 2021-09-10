d747e29114f05fd96c1d63a6c4f78d1450c0c7b0
ZopfGg <x.egea.es@gmail.com>
2021-09-10 10:11:21 +0200

    Initial commit

---
+++ complejos/src/main/java/com/example/complejos/Complejos.java
@@ -0,0 +1,33 @@
+/** Clase que representa un número complejo. */
+
+package com.example.complejos;
+
+class Complejo {
+
+    //declaración de atributos
+    private double real, imaginario;
+
+    //declaración de constructor
+    public Complejo(double real, double imaginario) {
+        this.real= real;
+        this.imaginario= imaginario;
+    }
+
+    //declaración de métodos
+    /** Transcribe el complejo a String.
+     * @returnun string con la parte entera y la imaginaria
+     */
+
+    public String toString() {
+        return real + "+"+ imaginario + "i";
+    }
+
+    /** Suma al complejo de este objeto otro complejo.
+     * @param v  el complejo que sumamos
+     */
+
+    public void suma(Complejo v) {
+        real = real + v.real;
+        imaginario = imaginario + v.imaginario;
+    }
+}
